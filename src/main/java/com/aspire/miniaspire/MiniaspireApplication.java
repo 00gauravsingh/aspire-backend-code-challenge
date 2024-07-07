7af9d21fdd2c05147018203cbb7cee99f325e324
Gaurav Singh <gauravsingh.iitd@gmail.com>
2024-07-07 12:41:04 +0530

    Initial Commit

---
+++ src/main/java/com/aspire/miniaspire/MiniaspireApplication.java
@@ -0,0 +1,13 @@
+package com.aspire.miniaspire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniaspireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniaspireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspire/miniaspire/MiniaspireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspire.miniaspire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniaspireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
